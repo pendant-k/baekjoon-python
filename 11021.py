1bff467a93f58bfd34959f9daf0089aba4102eec
pendant_k <behind8486@gmail.com>
2021-12-27 01:19:59 +0900

    chapter 3 almost done

---
+++ 11021.py
@@ -0,0 +1,4 @@
+T = int(input())
+for n in range(1, T+1):
+    a, b = map(int, input().split())
+    print(f"Case #{n}: {a+b}")
---
+++ 2742.py
@@ -0,0 +1,5 @@
+N = int(input())
+for order in range(N):
+    if(N >= 1):
+        print(f"{N}")
+        N -= 1
